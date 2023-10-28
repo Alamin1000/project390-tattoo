341ff3bd9fd4bc8e84f1c9c587f27fcee9de904f
alamin <alaminhosen44000@gmail.com>
2023-10-28 10:51:51 +0600

    1.00

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+  "use strict";
+})(jQuery);
+
+$(document).ready(function () {
+  // nice-select
+  $(".nice-select").niceSelect();
+
+  // preloader
+  $("#preloader").fadeOut(500);
+});
